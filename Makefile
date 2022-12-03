9a75e23779cbfad3700b3bc884af988576ef6599
James Liu <james@jamesliu.io>
2022-12-03 12:08:58 +1100

    Day 1 part 2

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ day:=day_$(DAY)
 input:=$(INPUT)
 
 build:
-	gcc $(day).c -o $(day).out
+	gcc $(day).c -g -O0 -Wall -Wextra -o $(day).out
 
 run: build
 	./$(day).out $(input)
--- day_1.c
+++ day_1.c
@@ -2,26 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+int comp (const void* elem1, const void* elem2) {
+    int f = *((int*)elem1);
+    int s = *((int*)elem2);
+    if (f > s) return -1;
+    if (f < s) return 1;
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         exit(1);
     }
 
-    int max_sum, curr_sum = 0;
+    int n_elves = 0;
+    int* sums = malloc(sizeof(int));
+    sums[0] = 0;
 
     const int LINE_SIZE = 6 + 1;
     char buf[LINE_SIZE];
-    FILE *input = fopen(argv[1], "r");
+    FILE* input = fopen(argv[1], "r");
     while (fgets(buf, LINE_SIZE, input) != NULL) {
-        // Reset counter for the next elf's calories.
-        if (strcmp(buf, "\n") == 0) {
-            if (curr_sum > max_sum) max_sum = curr_sum;
-            curr_sum = 0;
-        }
+        sums[n_elves] += atoi(buf);
 
-        curr_sum += atoi(buf);
+        // Reset counter and allocate space for the next elf's calories.
+        if (strcmp(buf, "\n") == 0) {
+            n_elves += 1;
+            sums = reallocarray(sums, n_elves+1, sizeof(int));
+            sums[n_elves] = 0;
+        }
     }
     fclose(input);
 
-    printf("%d\n", max_sum);
+    qsort(sums, n_elves, sizeof(int), comp);
+
+    printf("Part 1: %d\n", sums[0]);
+    printf("Part 2: %d\n", sums[0] + sums[1] + sums[2]);
 }
