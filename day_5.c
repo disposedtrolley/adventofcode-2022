0453e717373d572d92467309d73815d2289d1e9e
James Liu <james@jamesliu.io>
2022-12-06 22:18:56 +1100

    wip: Day 5 parse move instructions

--- day_5.c
+++ day_5.c
@@ -36,8 +36,34 @@ void parse_stack_row(const char row[], Stack stacks[]) {
     }
 }
 
-void parse_move_row(const char row[]) {
-    printf("len: %lu str: %s\n", strlen(row), row);
+void parse_move_row(const char row[], int instructions[3]) {
+    int instruction_idx = 0;
+    int n_spaces = 0;
+    char buf[2];
+    int buf_idx = 0;
+    for (size_t i = 5; i < strlen(row); i++){
+        if (row[i] == ' ') {
+            n_spaces++;
+
+            if (buf_idx != 0) {
+                // flush the number of crates and "from" stack
+                instructions[instruction_idx] = atoi(buf);
+                instruction_idx++;
+                buf_idx = 0;
+            }
+
+            continue;
+        }
+
+        // digits every 2 spaces
+        if (n_spaces == 0 || n_spaces == 2 || n_spaces == 4) {
+            buf[buf_idx] = row[i];
+            buf_idx++;
+        }
+
+        // flush the "to" stack
+        if (i == strlen(row)-1) instructions[instruction_idx] = atoi(buf);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -46,6 +72,9 @@ int main(int argc, char *argv[]) {
     }
 
     bool finished_parsing_stack_rows = false;
+
+    int instruction_idx = 0;
+    int instructions[600][3];
 
     const int N_STACKS = 9;
     Stack stacks[N_STACKS];
@@ -66,7 +95,8 @@ int main(int argc, char *argv[]) {
         if (!finished_parsing_stack_rows) {
             parse_stack_row(buf, stacks);
         } else if (strlen(buf) > 0 && buf[0] == 'm') {
-            parse_move_row(buf);
+            parse_move_row(buf, instructions[instruction_idx]);
+            instruction_idx++;
         }
     }
     fclose(input);
