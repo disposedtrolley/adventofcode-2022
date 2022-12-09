4cb45cd0d7a8d2ba0b6dfddb8433ec766c908f90
James Liu <james@jamesliu.io>
2022-12-09 12:37:19 +1100

    Day 6 part 1

--- day_6.c
+++ day_6.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -8,11 +9,29 @@ int main(int argc, char *argv[]) {
     }
 
     const int LINE_SIZE = 4096;
-    char buf[LINE_SIZE];
+    char buf[4096];
     FILE* input = fopen(argv[1], "r");
-    while (fgets(buf, LINE_SIZE, input) != NULL) {
-    }
+    fgets(buf, LINE_SIZE, input);
     fclose(input);
 
-    printf("%s\n", buf);
+    const int MARKER_LENGTH = 4;
+
+    int marker_start = 0;
+    for (size_t i = 0; i < LINE_SIZE; i++) {
+        if (i >= LINE_SIZE - MARKER_LENGTH) break;
+
+        bool has_duplicates = false;
+        for (size_t j = i; j < i+MARKER_LENGTH; j++) {
+            for (size_t k = j+1; k < j+MARKER_LENGTH; k++) {
+                if (buf[j] == buf[k]) has_duplicates = true;
+            }
+        }
+
+        if (!has_duplicates) {
+            marker_start = i;
+            break;
+        }
+    }
+
+    printf("%d\n", marker_start+MARKER_LENGTH);
 }
