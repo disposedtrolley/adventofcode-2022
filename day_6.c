7fef84ad0c5c19a59274ed13d164a793a6c7aef1
James Liu <james@jamesliu.io>
2022-12-09 12:01:46 +1100

    wip: Day 6 scaffolding

---
+++ day_6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        exit(1);
+    }
+
+    const int LINE_SIZE = 4096;
+    char buf[LINE_SIZE];
+    FILE* input = fopen(argv[1], "r");
+    while (fgets(buf, LINE_SIZE, input) != NULL) {
+    }
+    fclose(input);
+
+    printf("%s\n", buf);
+}
