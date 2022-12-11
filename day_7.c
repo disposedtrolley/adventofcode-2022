c0e1b1357c16f688ce075e174ebd57bddc75253b
James Liu <james@jamesliu.io>
2022-12-11 14:34:31 +1100

    Day 7 part 1

--- day_7.c
+++ day_7.c
@@ -7,7 +7,13 @@
 
 #define LINE_SIZE 100
 #define DIRNAME_LENGTH 100
-#define PATH_LENGTH 20
+#define DEPTH 20
+
+#define MAX_SIZE 100000
+
+bool includes(const char *str, const char *substr) {
+    return strstr(str, substr) != NULL;
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -15,7 +21,7 @@ int main(int argc, char *argv[]) {
     }
 
     ht *dirs = ht_create();
-    Stack path = stack_new(PATH_LENGTH);
+    Stack path = stack_new(DEPTH);
 
     char buf[LINE_SIZE];
     FILE* input = fopen(argv[1], "r");
@@ -39,7 +45,7 @@ int main(int argc, char *argv[]) {
             } else {
                 stack_push(&path, val);
 
-                char joined[DIRNAME_LENGTH * 10] = {0};
+                char joined[DIRNAME_LENGTH * DEPTH] = {0};
                 stack_join(&path, "/", joined);
                 ht_set(dirs, joined, 0);
             }
@@ -53,7 +59,7 @@ int main(int argc, char *argv[]) {
             char *val = malloc(sizeof(char));
             strcpy(val, tok);
 
-            char joined[DIRNAME_LENGTH * 10] = {0};
+            char joined[DIRNAME_LENGTH * DEPTH] = {0};
             stack_join(&path, "/", joined);
             int curr_size = ht_get(dirs, joined);
             ht_set(dirs, joined, curr_size + atoi(val));
@@ -61,11 +67,23 @@ int main(int argc, char *argv[]) {
     }
     fclose(input);
 
+    int sum_sizes = 0;
+
     hti iterator = ht_iterator(dirs);
     while (ht_next(&iterator)) {
-        printf("key: %s value: %d\n", iterator.key, iterator.value);
+        int total_size = iterator.value;
+
+        hti jiterator = ht_iterator(dirs);
+        while (ht_next(&jiterator)) {
+            if (!includes(jiterator.key, iterator.key) || strcmp(iterator.key, jiterator.key) == 0) continue;
+            total_size += jiterator.value;
+        }
+
+        if (total_size <= MAX_SIZE) sum_sizes += total_size;
     }
 
     stack_free(&path);
     ht_destroy(dirs);
+
+    printf("Part 1: %d\n", sum_sizes);
 }
